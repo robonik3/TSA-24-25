5c04a8a46594afe87a6f620b2f1bcea7bcb396cc
ghimirerush <ghimirerush@gmail.com>
2024-11-30 11:46:58 -0500

    11/30 Game Changes
    3rd meeting, made level and camera movement

---
+++ TSA-VGD-24-25/Assets/CameraFollow.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec4e19ce19f187ee539a4f36c6eb59fcac51580561f98be38c6b5a7a33f4bf92
+size 500
---
+++ TSA-VGD-24-25/Assets/CameraZoom.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:23e8106cba035235ff4e9d733e5ccebb1e51c598219e582d369c6ab9659cc456
+size 834
