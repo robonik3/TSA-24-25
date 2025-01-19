a38c96926e23c060335aa8c2de3765ea1e496b4e
Nik <136362055+robonik3@users.noreply.github.com>
2025-01-18 19:50:56 -0500

    Fixes
    Fixed the delay in the transition from jump animation to idle or moving and made the animation idle when a player is standing on top of another player. Also fixed the battery indicator scaling.

--- TSA-VGD-24-25/Assets/Scripts/CameraFollow.cs
+++ TSA-VGD-24-25/Assets/Scripts/CameraFollow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72925cdadb2f56d298a55dc6865f599058f63276991a44d3b5c4e555873576a1
-size 8719
+oid sha256:bf85baf739a068528c8ee91c683a26eb2032430fda654ebb4e192fb2eca864a0
+size 8836
--- TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
+++ TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7f18dfa71d73747b63a376860b3ef5777622758fbbb8fa8deb1f537b1519c60
-size 2494
+oid sha256:9fe81651fc6f27a7207c0e69472c5872f309d661b86c891d10e7a010f4adf341
+size 2662
