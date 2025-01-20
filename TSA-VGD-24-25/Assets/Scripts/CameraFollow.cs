470a2cf0f0a2c9bc47942adb51be65a0914b2b4e
Nik <136362055+robonik3@users.noreply.github.com>
2025-01-19 21:20:45 -0500

    Update CameraFollow.cs

--- TSA-VGD-24-25/Assets/Scripts/CameraFollow.cs
+++ TSA-VGD-24-25/Assets/Scripts/CameraFollow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3128945bf336437d77a9dbfa0eb941b11c2d7a1a64bb6c4fa3e259cca2e320ad
-size 4014
+oid sha256:3b1c9e219e74816ca816bce65c9d540cfd9f8711169a5d57a0f33948846cce9c
+size 4013
