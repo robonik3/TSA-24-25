54ff274b2c39820b97ac56094d79b1857f558e68
Nik <136362055+robonik3@users.noreply.github.com>
2025-01-28 11:40:39 -0500

    Flash
    The lightbulb can flash now and i also made the animations smoother

--- TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
+++ TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:397b23c7f9805d4d5edbf1636ab4a9b00865b594938a230c3a912eed9d09389a
-size 4032
+oid sha256:97f28e8204a36d146c637da75e9e495baca3802c2ca30c825fd5fe95476d9c64
+size 4391
