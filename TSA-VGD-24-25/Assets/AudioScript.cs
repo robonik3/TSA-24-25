f3a3ca50a31e66b8fedaae3b4b32f55189a063be
Nik <136362055+robonik3@users.noreply.github.com>
2025-02-08 19:33:23 -0500

    QOL Changes
    Small fixes and additions including more text to teach the players, water now kills the players, and the audio doesn't restart if the game is restarted after game over.

---
+++ TSA-VGD-24-25/Assets/AudioScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b49c6d14483024a0f76cc34a6f20079212d60ce4755c028f30b148354691935
+size 798
--- TSA-VGD-24-25/Assets/Scripts/Player1Movement.cs
+++ TSA-VGD-24-25/Assets/Scripts/Player1Movement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:053dc01a96f56b8915ef247db0af9d50840c17c8d9028c924af69727fccaefbd
-size 3228
+oid sha256:4667a6d6152a32a870a84647a5a1e4e06b77a8e7585c40e8a9b640b362a1abd7
+size 3451
