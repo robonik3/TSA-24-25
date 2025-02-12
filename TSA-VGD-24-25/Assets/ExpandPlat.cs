79c81bedb218884bfab1af3ec3f348cddd74ec50
Nik <136362055+robonik3@users.noreply.github.com>
2025-02-12 12:57:17 -0500

    Generator and Level two
    Added generator which switches on once bat and bulb go inside it, and also made some changes to level two

---
+++ TSA-VGD-24-25/Assets/ExpandPlat.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa97b5faa5024541ee1485756d23b4f27fd453ba43b0d8f1150a23f523e3c46b
+size 1102
---
+++ TSA-VGD-24-25/Assets/GenScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:23496edd17959841d178f902a60de33a571c99b50920f8445957c8cc030b870e
+size 1552
