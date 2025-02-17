c5320e7402448559acc09c1ac43f319b8f12975c
MOMANDZENTV <momandzentv@gmail.com>
2025-02-17 13:28:35 -0500

    Made Ice slippery

--- TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
+++ TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:face1defcad436e4734334ed658f74c89759ed927e358f951add11826fc7a6d8
-size 7071
+oid sha256:ea23e44bd7150a0e086047ea706d009d12e2348d6faf9e4a45cc9ee7e4d94c95
+size 5896
