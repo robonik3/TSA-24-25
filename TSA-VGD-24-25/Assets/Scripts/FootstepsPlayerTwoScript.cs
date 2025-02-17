e844681bb95d9058882ee5dd67642d4128a20968
ghimirerush <ghimirerush@gmail.com>
2025-02-17 11:56:56 -0500

    Revert "commit"
    This reverts commit 651913355066da3de4d3deed07462b2226e697ac.

--- TSA-VGD-24-25/Assets/Scripts/FootstepsPlayerTwoScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:00bc42826048ee40979324468bcbceab4779a69ee6fb50b080d4088561faed27
-size 732
--- TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
+++ TSA-VGD-24-25/Assets/Scripts/Player2Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42d752d08d355f604ec1107f74b342ee67f9d2a2c34390a09da562bcebb15355
-size 272
+oid sha256:012387e037f7414578f8ac53fecbebb2903b6763eccca107b22c6b4e75c3b078
+size 6979
