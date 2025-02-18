0c33ee4d3795278ce4c831d010cee6221e4f5da6
ghimirerush <ghimirerush@gmail.com>
2025-02-17 21:22:11 -0500

    Build

--- TSA-VGD-24-25/Assets/LevelSelectScreen.cs
+++ TSA-VGD-24-25/Assets/LevelSelectScreen.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fd6e021e4ee1ce54af8eb90f2800d37b8a5a3243ecd1d1e7906f7e2549bd633
-size 834
+oid sha256:a59ef7bab7e3dfe81356e6b6d8193ac0d26475ae64f33c92344397e7cfd091a8
+size 800
--- TSA-VGD-24-25/Assets/TransitionManagerScript.cs
+++ TSA-VGD-24-25/Assets/TransitionManagerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:418fd82e6a6cfa32353be44c1d3636953bda8111db073e057c47155dd2d10e60
-size 979
+oid sha256:30a2f2f47098e2136d9e5e772d0778e83c2f6ae2f8fe508fcee41609d82de8f9
+size 936
