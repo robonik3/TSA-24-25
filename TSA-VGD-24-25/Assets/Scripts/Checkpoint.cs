1db476ad2e6cc5b6c7e896f22b9b2c8b795f9ca1
MOMANDZENTV <momandzentv@gmail.com>
2025-02-19 21:35:27 -0500

    Working Checkpoints

--- TSA-VGD-24-25/Assets/Scripts/Checkpoint.cs
+++ TSA-VGD-24-25/Assets/Scripts/Checkpoint.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2045ac5dd517d045e07f53d3362f75f47c6fbb8971421dcfe55adafa814455ef
-size 652
+oid sha256:b178761a78efef865a8c830cc07b2dad1618571654c7f517e19e20a745a639b4
+size 1021
--- TSA-VGD-24-25/Assets/Scripts/GenScript.cs
+++ TSA-VGD-24-25/Assets/Scripts/GenScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df1bc32eb33350aaebc71e9e6d4cd5a17dd394c415161e042525974367b2079
-size 2174
+oid sha256:8d50ed02efb43ecee56f610942f7269ac51acc17f41ee40f2c73dcb62457259e
+size 2102
