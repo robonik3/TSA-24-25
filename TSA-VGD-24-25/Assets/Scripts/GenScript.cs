7a2055ae92c4f2c42b787359f63f34b5948ad3c7
MOMANDZENTV <momandzentv@gmail.com>
2025-02-20 21:10:24 -0500

    Fixed checkpoint persisting bug

--- TSA-VGD-24-25/Assets/Scripts/GenScript.cs
+++ TSA-VGD-24-25/Assets/Scripts/GenScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bf6fd766358b06d3281f86dcbcec1303d1d3ad74cc01be8dae1d5df1ee6aff9
-size 2118
+oid sha256:e0462bdcc5d873e7c694dcfc4d50a14498efa17465e4ffd4ceb6808a446331d3
+size 2102
